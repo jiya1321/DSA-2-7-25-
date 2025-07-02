743529396b100e1b9ca20ed71f2d06e81cdc6fc2
jiya1321 <jiyaagnihotri459@gmail.com>
2025-07-02 21:20:28 +0530

    Create try.cpp

---
+++ try.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"enter your no:";
+
+    cin>>num;
+
+    if(num%2==0){
+        cout<<num<<" is an even. "<<endl;
+    }
+    else{
+        cout<<num<<" is an odd. "<<endl;
+    }
+return 0;
+}

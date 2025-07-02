3f656fa7b71384e692e28e32084db27f69ac2a1f
jiya1321 <jiyaagnihotri459@gmail.com>
2025-07-02 21:20:03 +0530

    Create sumOf2numbers.cpp

---
+++ sumOf2numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a,int b){
+        return a+b;
+    }
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int sum1 = sum(a,b);
+    cout<<sum1;
+
+}
